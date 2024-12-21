e7282261cdaffeb8b1a5440f0f6edfab6942b33a
mgradyn <mgradyn@gmail.com>
2024-12-21 21:42:12 +0700

    adj: remove misssing arguments

--- src/test.py
+++ src/test.py
@@ -39,7 +39,7 @@ def main():
     # Dataset Preparation
     dataset = DatasetUtils.load_dataset()
     dataset = DatasetUtils.remove_class(dataset, split="train", class_label=5) # remove ambigious class (class 5)
-    dataset = DatasetUtils.split_dataset(dataset, train_size=0.8, val_size=0.1, test_size=0.1)
+    dataset = DatasetUtils.split_dataset(dataset)
 
     # create DataLoaders
     _, val_test_transform = DatasetUtils.get_transforms()
